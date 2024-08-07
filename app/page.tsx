1ac0869c72c92dff75cb2436201033f194a6ec49
kmaengggong <kmaengggong@gmail.com>
2024-08-07 18:58:32 +0900

    GEO: 1.1 - 2.4

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "../components/navigation";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Hello Next.js!</h1>
+        </div>
+    );
+}
---
+++ components/navigation.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+export default function Navigation() {
+    const path = usePathname();
+    const [count, setCount] = useState(0);
+    
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link> {path === "/" ? "🎈" : ""}
+                </li>
+                <li>
+                    <Link href="/about-us">About Us</Link> {path === "/about-us" ? "🎈" : ""}
+                </li>
+                <li>
+                    <button onClick={() => setCount(c => c + 1)}>
+                        {count}
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
+}
