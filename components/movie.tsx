2a2f004fa1dbba78933dbb194de112af1858badf
kmaengggong <kmaengggong@gmail.com>
2024-08-20 13:22:43 +0900

    GEO: prefetch 추가

--- components/movie.tsx
+++ components/movie.tsx
@@ -19,7 +19,7 @@ export default function Movie({title, id, poster_path}: IMovieProps) {
     return (
         <div className={styles.movie}>
             <img src={poster_path} alt={title} onClick={onClick}/>
-            <Link href={`/movies/${id}`}>{title}</Link>
+            <Link prefetch href={`/movies/${id}`}>{title}</Link>
         </div>
     );
 }
