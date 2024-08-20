204d6e1e8d092c8f05e91cf3a3a7b91eec980a05
kmaengggong <kmaengggong@gmail.com>
2024-08-20 13:14:41 +0900

    GEO: API_URL 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,11 @@
 import { Metadata } from "next";
 import Movie from "../../components/movie";
 import styles from "../../styles/home.module.css";
+import { API_URL } from "../constants";
 
 export const metadata: Metadata = {
     title: "Home"
 };
-
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
     await new Promise((resolve) => setTimeout(resolve, 1000));
